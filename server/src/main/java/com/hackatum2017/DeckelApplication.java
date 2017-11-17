e9bcdd63bddcd371bd9f50ff75817ad57cac66ee
apodznoev <apodznoev@yandex.ru>
2017-11-17 22:45:16 +0100

    Added basic Spring boot server

---
+++ server/src/main/java/com/hackatum2017/DeckelApplication.java
@@ -0,0 +1,12 @@
+package com.hackatum2017;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeckelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeckelApplication.class, args);
+	}
+}
---
+++ server/src/main/java/com/hackatum2017/controller/response/HelloResponse.java
@@ -0,0 +1,17 @@
+package com.hackatum2017.controller.response;
+
+/**
+ * Created by apodznoev
+ * date 25.10.2017.
+ */
+public class HelloResponse {
+    private final String helloMsg;
+
+    public HelloResponse(String helloMsg) {
+        this.helloMsg = helloMsg;
+    }
+
+    public String getHelloMsg() {
+        return helloMsg;
+    }
+}
