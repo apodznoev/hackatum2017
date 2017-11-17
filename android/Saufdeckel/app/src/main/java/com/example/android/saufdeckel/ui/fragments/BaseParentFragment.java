4d3bde7cfb25c6bf4bc0f3880879edcc025db6ed
Lars Braitinger <lars.braitinger@gmail.com>
2017-11-17 23:34:18 +0100

    add splash screen and prepare project

---
+++ android/Saufdeckel/app/src/main/java/com/example/android/saufdeckel/ui/fragments/BaseParentFragment.java
@@ -0,0 +1,29 @@
+package com.example.android.saufdeckel.ui.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+import com.example.android.saufdeckel.ui.MainActivity;
+
+
+public abstract class BaseParentFragment extends Fragment {
+    public MainActivity parentActivity;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        parentActivity = (MainActivity)getActivity();
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        setNavTitle();
+    }
+
+    public void setNavTitle() {
+        if ( parentActivity != null )
+            parentActivity.setTitle(getNavTitle());
+    }
+    public abstract String getNavTitle();
+}
